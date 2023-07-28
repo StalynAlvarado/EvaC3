8815be1db43194cdcdb205cf4a49b3ded31dde6d
StalynAlvarado <136040944+StalynAlvarado@users.noreply.github.com>
2023-07-28 03:08:25 +0000

    First

---
+++ demo/src/main/java/com/example/demo/InstrumentoRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(collectionResourceRel="instrumento",path = "instrumento")
+public interface InstrumentoRepository extends PagingAndSortingRepository<Instrumento, Integer>,CrudRepository<Instrumento, Integer>{
+    
+    List<Instrumento> findByNombre(@Param("nombre")String nombre);
+}
